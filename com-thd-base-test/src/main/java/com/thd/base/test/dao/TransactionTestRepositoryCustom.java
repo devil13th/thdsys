66bbcb2b5a1b107a258d7b76c9e3444c2ed654c6
十三妖 <181907667@qq.com>
2019-01-03 21:57:49 +0800

    模块更名

--- com-thd-base-test/src/main/java/com/thd/base/test/dao/TransactionTestRepositoryCustom.java
+++
@@ -1,7 +0,0 @@
-package com.thd.base.test.dao;
-
-import java.util.List;
-
-public interface TransactionTestRepositoryCustom {
-	public List test();
-}
--- com-thd-common-infrastructure/src/main/java/com/thd/common/infrastructure/dao/SysDicPubDao.java
+++
@@ -1,8 +0,0 @@
-package com.thd.common.infrastructure.dao;
-
-import com.thd.common.infrastructure.pojo.SysDicPub;
-import com.thd.core.dao.BaseRepository;
-
-public interface SysDicPubDao extends BaseRepository<SysDicPub, String> {
-
-}
