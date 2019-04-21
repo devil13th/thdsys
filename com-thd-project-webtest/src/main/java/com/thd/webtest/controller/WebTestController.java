3b73bd960fe80811348759651e2e86e3c2a33b2b
十三妖 <181907667@qq.com>
2019-04-22 00:27:06 +0800

    添加webTestController

--- com-thd-project-webtest/src/main/java/com/thd/webtest/controller/WebTestController.java
+++ com-thd-project-webtest/src/main/java/com/thd/webtest/controller/WebTestController.java
@@ -1,18 +1,36 @@
 package com.thd.webtest.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.thd.core.bean.ResponseBean;
 import com.thd.core.controller.BaseController;
+import com.thd.webtest.service.WebTestService;
 @RestController
+@RequestMapping(value="/webtest")
 public class WebTestController extends BaseController {
-	@RequestMapping(value="/webtest/text")
+	@Autowired
+	private WebTestService webTestService;
+	@RequestMapping(value="/text")
 	public ResponseEntity<ResponseBean> test(){
 		System.out.println("SUCCESS");
 		ResponseBean rb = new ResponseBean();
 		rb.setResult("success");
 		return rb.success();
+	}
+	
+	
+	@RequestMapping(value="/testDbLock")
+	public ResponseEntity<ResponseBean> testDbLock(){
+		System.out.println("testDbLock");
+		
+		webTestService.testDbLock();
+		
+		ResponseBean rb = new ResponseBean();
+		rb.setResult("success");
+		return rb.success();
+		
 	}
 }
